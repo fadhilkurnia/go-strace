384916dd29335374821fd04b5092d58166105016
Fadhil Kurnia <fadhilimamk@gmail.com>
2021-09-09 15:45:28 -0400

    init go module

---
+++ go.mod
@@ -0,0 +1,3 @@
+module cs.umass.edu/fikurnia/go-strace
+
+go 1.15
--- sample.go
+++ sample.go
@@ -8,7 +8,7 @@ import (
 	"os/signal"
 	"syscall"
 
-	"github.com/maebashi/go-strace/strace"
+	"cs.umass.edu/fikurnia/go-strace/strace"
 )
 
 func main() {
